589dcc2ec6a5f7f5cc44392d0f3ced2cb8666e78
mulder2@gmx.de <mulder2@gmx.de@8a597620-9bd1-4498-a720-a9593e25b6ad>
2011-07-31 18:55:02 +0000

    [wma2wav] Added some more comments to the header file.
    git-svn-id: svn://svn.code.sf.net/p/muldersoft/code/trunk/Utils/wma2wav@248 8a597620-9bd1-4498-a720-a9593e25b6ad

--- wma2wav/WmaReader.h
+++ wma2wav/WmaReader.h
@@ -31,18 +31,98 @@ public:
 	CWmaReader(void);
 	~CWmaReader(void);
 
+	/* getRuntimeVersion()
+	/*   This function can be used to detect the version of the Windows Media Runtime that is used.
+	/*   The 'version' parameter points to a pre-allocated buffer that will receive the version string. The 'size' parameter specifies the size of the buffer, in wchar_t's.
+	/*   Returns TRUE if successfull, FALSE otherwise.
+	 */
 	bool getRuntimeVersion(wchar_t *version, size_t size);
+
+	/* isValid()
+	/*   This function checks wether a given file is a valid ASF file that can be processed. Only valid files can be processed!
+	/*   The 'filename' parameter points to a null-terminated string that specifies the path to the file that should be checked.
+	/*   Returns TRUE if the specified file is readable and valid, FALSE otherwise.
+	 */
 	bool isValid(const wchar_t *filename);
+
+	/* isProtected()
+	/*   This function checks wether a given ASF file is "protected" by DRM. If file is protected, it cannot be processed!
+	/*   The 'filename' parameter points to a null-terminated string that specifies the path to the file that should be checked.
+	/*   Returns TRUE if the specified file is a protected ASF file, FALSE otherwise.
+	 */
 	bool isProtected(const wchar_t *filename);
+
+	/* open()
+	/*   This function opens the given ASF file for reading. Can only succeed, if no file is not open yet or the previous file was closed!
+	/*   The 'filename' parameter points to a null-terminated string that specifies the path to the file that should be opened. Only valid and unprotected ASF files can be opened.
+	/*   Returns TRUE if the specified file was opened successfully, FALSE otherwise.
+	 */
 	bool open(const wchar_t *filename);
+
+	/* close()
+	/*   This function closes the file that is currently open. It does nothing, if no file is currently open.
+	 */
 	void close(void);
+
+	/* analyze()
+	/*   This function analyses the input ASF file and tries to find a suitable audio stream.
+	/*   The 'format' parameter points to a pre-allocated WAVEFORMATEX structure that will receive the "native" format of the audio stream - only valid if the function succeeded.
+	/*   Note that the format specification returned by this function generally is NOT identical to the format of the audio samples returned by the getNextSample() function!
+	/*   Returns TRUE if the analysis completed successfully and an audio stream was found, FALSE otherwise.
+	 */
 	bool analyze(WAVEFORMATEX *format);
+	
+	/* configureOutput()
+	/*   This function changes the output format, i.e. the format of the samples returned by getNextSample().
+	/*   The 'format' parameter points to a pre-allocated WAVEFORMATEX structure that contains the new output format specification.
+	/*   Returns TRUE if the output format was changed successfully, FALSE otherwise.
+	/*   If the function succeeded, call getOutputFormat() afterwards in order to obtain the new output format!
+	 */
 	bool configureOutput(WAVEFORMATEX *format);
+	
+	/* getOutputFormat()
+	/*   This function determines the current output format, i.e. the format of the samples returned by getNextSample().
+	/*   The 'format' parameter points to a pre-allocated WAVEFORMATEX structure that will receive the output format of the audio stream - only valid if the function succeeded.
+	/*   Returns TRUE if the output format was read successfully, FALSE otherwise.
+	 */
 	bool getOutputFormat(WAVEFORMATEX *format);
+	
+	/* getSampleSize()
+	/*   This function determines the maximum sample size. The size of the output buffer passed to getNextSample() must have at least this size!
+	/*   Returns the maximum sample size, in bytes. If the funcation failed, it returns ZERO.
+	 */
 	size_t getSampleSize(void);
+	
+	/* getDuration()
+	/*   This function determines the duration of the media that is currently open.
+	/*   Returns the duration, in seconds. If the funcation failed, the return value is nagative.
+	 */
 	double getDuration(void);
+	
+	/* getCodecInfo()
+	/*   This function determines the Codec name and the Codec info of the audio stream, if that information is available.
+	/*   The 'codecName' parameter points to a pre-allocated buffer that will receive the Codec name. The size parameter specifies the buffer size, in wchar_t's.
+	/*   The 'codecInfo' parameter points to a pre-allocated buffer that will receive the Codec name. The size parameter specifies the buffer size, in wchar_t's.
+	/*   Returns TRUE if the information was read successfully, FALSE otherwise.
+	 */
 	bool getCodecInfo(wchar_t *codecName, wchar_t *codecInfo, size_t size);
+	
+	/* getTitle()
+	/*   This function determines the title of the that is currently open, if that information is available.
+	/*   The 'title' parameter points to a pre-allocated buffer that will receive the title of the media. The size parameter specifies the buffer size, in wchar_t's.
+	/*   Returns TRUE if the title was read successfully, FALSE otherwise.
+	 */
 	bool getTitle(wchar_t *title, size_t size);
+	
+	/* getNextSample()
+	/*   This function reads the next chunk of audio samples from the input audio stream.
+	/*   The 'output' parameter points to a pre-allocated buffer that will received the audio samples. The 'size' parameter specifies the size of the output buffer, in bytes.
+	/*   The 'length' parameter points to a variable the will receive the number of bytes that were written into the output buffer. ZERO indicates the end of the stream.
+	/*   The optional 'timeStamp' and 'sampleDuration' parameters point to variables that will receive the timestamp and the duration, in seconds, of the samples that were read.
+	/*   The required minimum output buffer size, in bytes, can be determined by calling the getSampleSize() function. If the output buffer is smaller, samples may be dropped!
+	/*   The format specification of the audio samples returned by this function can be determined by calling the getOutputFormat() function.
+	/*   Returns TRUE if the samples were read successfully (even if the end of the stream was reached), FALSE otherwise.
+	 */
 	bool getNextSample(BYTE *output, const size_t size, size_t *length, double *timeStamp = NULL, double *sampleDuration = NULL);
 
 private:
@@ -58,4 +138,3 @@ private:
 	bool _findAudioStream(WAVEFORMATEX *format);
 	bool _testAudioStream(WAVEFORMATEX *format, const WORD idx, IWMStreamConfig *pIWMStreamConfig);
 };
-
